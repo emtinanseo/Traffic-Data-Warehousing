e37482749f78fc14d68201c05d64ac777c31b98c
emtinanseo <emtinan.s.e.osman.com>
2023-07-05 00:19:27 +0300

    added a dbt model vehicle_summary

---
+++ dbt/traffic/models/vehicles_summary.sql
@@ -0,0 +1,11 @@
+WITH vehicle_summary AS (
+    SELECT
+        type,
+        COUNT(track_id) AS count,
+        AVG(traveled_d) AS avg_distance,
+        AVG(avg_speed) AS avg_speed
+    FROM vehicles
+    GROUP BY type
+)
+
+SELECT * FROM vehicle_summary
