02a873ef5c8b26b915860313a70cd0ddea689760
emtinanseo <emtinan.s.e.osman.com>
2023-07-29 18:48:02 +0300

    split job into multiple tasks to reduce run time

--- dags/preprocess_load_data.py
+++ dags/preprocess_load_data.py
@@ -1,7 +1,12 @@
 from airflow.decorators import dag, task
 from datetime import datetime
+import pandas as pd
 from pathlib import Path
 import sys
+import config
+
+python_path = config.python_path
+dir = config.dir
 
 sys.path.append(str(Path(__file__).parent.parent))
 from scripts.read_data import Reader
@@ -20,22 +25,28 @@ def preprocess_load_data():
       - load the data into 2 tables in the database
     """
     
-    @task(multiple_outputs=True)
-    def preprocess_data(data_file:str = "data/20181024_d1_0900_0930.csv"):
-    # def preprocess_data(data_file:str = "data/20181024_d1_0830_0900.csv"):
+    @task.external_python(python=python_path)
+    def preprocess_data(data_file:str) -> dict[str, pd.DataFrame]:
+    # def preprocess(data_file:str = "data/20181024_d1_0830_0900.csv"):
         reader = Reader()
         vehicles, trajectories = reader.data_dfs(data_file)
 
-        return {"vehicles": vehicles," trajectories": trajectories}
-    
-    @task()
-    def load_data(df):
-        db = Database()
-        db.vehicle_dataframe_to_sql(df["vehicles"])
-        db.trajectory_dataframe_to_sql(df["trajectories"])
+        return {"vehicles": vehicles, "trajectories":trajectories}
 
+    @task.external_python(python=python_path)
+    def load_vehicles(df:pd.DataFrame):  
+        db = Database()
+        db.vehicle_dataframe_to_sql(df)
+
+    @task.external_python(python=python_path)
+    def load_traj(df:pd.DataFrame):
+        db = Database()
+        db.trajectory_dataframe_to_sql(df)
    
-    df = preprocess_data()
-    load_data(df)
+
+    data_file = dir + "/data/20181024_d1_0830_0900.csv"
+    dfs = preprocess_data(data_file)
+    load_vehicles(dfs["vehicles"])
+    load_traj(dfs["trajectories"])
 
 preprocess_load_data()
